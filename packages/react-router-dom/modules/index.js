5bfe689562fc79a15e854509317f7f59f98d0877
Mark Vayngrib <mark@tradle.io>
2020-02-21 08:20:18 -0500

    fix imports to use @exodus/react-router

--- packages/react-router-dom/modules/index.js
+++ packages/react-router-dom/modules/index.js
@@ -1,4 +1,4 @@
-export * from "react-router";
+export * from "@exodus/react-router";
 
 export { default as BrowserRouter } from "./BrowserRouter";
 export { default as HashRouter } from "./HashRouter";
